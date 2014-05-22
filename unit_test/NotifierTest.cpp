c3efe64451336bb9a9dbc4b9c9cdd9724eee62cf
Dawid Drozd <drozddawid.uam@gmail.com>
2014-05-22 11:58:27 +0200

    Spliting tests

--- unit_test/NotifierTest.cpp
+++ unit_test/NotifierTest.cpp
@@ -23,12 +23,9 @@ MAKE_NOTIFICATION( Test4, float );
 
 struct TestClazz : CCObject
 {
-	TestClazz() :
-		deliveredCount( 0 )
-	{
-	}
+	TestClazz(){}
 
-	int deliveredCount;
+	int deliveredCount = 0;
 
 	void onOldNotification( CCObject* pObject )
 	{
@@ -63,7 +60,6 @@ struct TestClazz : CCObject
 	{
 		deliveredCount += value;
 	}
-
 };
 
 TEST( Notifier, TestOfNotification )
@@ -199,69 +195,6 @@ TEST( Notifier, TestOfNotificationChangesStack )
 	EXPECT_EQ( 0, notifier.getListenersCount( getNotificationTest2().tag ) );
 }
 
-
-MAKE_NOTIFICATION( TestUseAfterRelease );
-
-class TestClazzReleased : public CCNode
-{
-public:
-	TestClazzReleased( Notifier& notifier ) :
-		m_pNotifier( notifier )
-	{
-		notifier.addNotification( getNotificationTestUseAfterRelease(), Utils::makeCallback( this,
-								  &TestClazzReleased::onCall ) );
-	}
-
-	virtual ~TestClazzReleased()
-	{
-		m_pNotifier.removeAllForObject( this );
-	}
-
-	void onCall()
-	{
-		EXPECT_GT( retainCount() , 0 );  //This object is released?
-		CCLOG( "Hello!" );
-	}
-
-private:
-	Notifier& m_pNotifier;
-};
-
-TEST( Notifier, DISABLED_TestOfNotificationUseAfterRelease )
-{
-	Notifier notifier;
-
-	CCNode* pHolder = new CCNode();
-	pHolder->init();
-	notifier.addNotification( getNotificationTestUseAfterRelease(), Utils::makeCallback( pHolder,
-							  &CCNode::removeAllChildren ) );
-
-	TestClazzReleased* pReleasedNode = new TestClazzReleased( notifier );
-	pReleasedNode->init();
-
-	EXPECT_EQ( 1, pReleasedNode->retainCount() );
-	EXPECT_EQ( 1, pHolder->retainCount() );
-
-	pHolder->addChild( pReleasedNode );
-
-	EXPECT_EQ( 2, pReleasedNode->retainCount() );
-	EXPECT_EQ( 1, pHolder->retainCount() );
-
-	pReleasedNode->release();
-
-	EXPECT_EQ( 1, pReleasedNode->retainCount() );
-	EXPECT_EQ( 1, pHolder->retainCount() );
-
-	EXPECT_EQ( 1, pReleasedNode->retainCount() );
-	EXPECT_EQ( 1, pHolder->retainCount() );
-
-	//Now when we notify we have problem. We have use after release.
-	//It should crash
-	notifier.notify( getNotificationTestUseAfterRelease() );
-
-	pHolder->release();
-}
-
 TEST( Notifier, TestOfNotificationChangesStackDoubleCommands )
 {
 	Notifier notifier;
@@ -327,202 +260,66 @@ TEST( Notifier, TestOfNotificationChangesStackDoubleCommands )
 	EXPECT_EQ( 1, notifier.getListenersCount( getNotificationTest2().tag ) );
 }
 
-TEST( Notifier, DISABLED_SpeedTestNotify )
+
+MAKE_NOTIFICATION( TestUseAfterRelease );
+
+class TestClazzReleased : public CCNode
 {
-	Notifier* pNotifier = new Notifier();
-	//We want fresh notification center
-	CCNotificationCenter::sharedNotificationCenter()->purgeNotificationCenter();
-	CCNotificationCenter* pCenter =
-		CCNotificationCenter::sharedNotificationCenter();
-
-	int* pTab = new int[rand() % 1000 + 100];
-	TestClazz* pTestClazz = new TestClazz();
-	delete pTab;
+public:
+	TestClazzReleased( Notifier& notifier ) :
+		m_pNotifier( notifier )
 	{
-		CC_PROFILER_PURGE_ALL();
-		pNotifier->addNotification( getNotificationTest4(),
-									Utils::makeCallback( pTestClazz, &TestClazz::onDeliverNotification2 ) );
-		pCenter->addObserver( pTestClazz,
-							  callfuncO_selector( TestClazz::onOldNotificationFloat ), OLD_NOTIFICATION_4,
-							  nullptr );
-
-		CCFloat floatValue( 22.3F );
-
-		for( int i = 0; i < 1000000; ++i )
-		{
-			CC_PROFILER_START( "Notifier_1kk_notify_to_1_listener" );
-			pNotifier->notify( getNotificationTest4(), 22.3F );
-			CC_PROFILER_STOP( "Notifier_1kk_notify_to_1_listener" );
-
-
-			CC_PROFILER_START( "CCNotificationCenter_1kk_notify_to_1_listener" );
-			pCenter->postNotification( OLD_NOTIFICATION_4, &floatValue );
-			CC_PROFILER_STOP( "CCNotificationCenter_1kk_notify_to_1_listener" );
-		}
-
-		CC_PROFILER_DISPLAY_TIMERS();
+		notifier.addNotification( getNotificationTestUseAfterRelease(), Utils::makeCallback( this,
+								  &TestClazzReleased::onCall ) );
 	}
-	CC_SAFE_DELETE( pTestClazz );
+
+	virtual ~TestClazzReleased()
+	{
+		m_pNotifier.removeAllForObject( this );
+	}
+
+	void onCall()
+	{
+		EXPECT_GT( retainCount() , 0 );  //This object is released?
+		CCLOG( "Hello!" );
+	}
+
+private:
+	Notifier& m_pNotifier;
+};
+
+TEST( Notifier, DISABLED_TestOfNotificationUseAfterRelease )
+{
+	Notifier notifier;
+
+	CCNode* pHolder = new CCNode();
+	pHolder->init();
+	notifier.addNotification( getNotificationTestUseAfterRelease(), Utils::makeCallback( pHolder,
+							  &CCNode::removeAllChildren ) );
+
+	TestClazzReleased* pReleasedNode = new TestClazzReleased( notifier );
+	pReleasedNode->init();
+
+	EXPECT_EQ( 1, pReleasedNode->retainCount() );
+	EXPECT_EQ( 1, pHolder->retainCount() );
+
+	pHolder->addChild( pReleasedNode );
+
+	EXPECT_EQ( 2, pReleasedNode->retainCount() );
+	EXPECT_EQ( 1, pHolder->retainCount() );
+
+	pReleasedNode->release();
+
+	EXPECT_EQ( 1, pReleasedNode->retainCount() );
+	EXPECT_EQ( 1, pHolder->retainCount() );
+
+	EXPECT_EQ( 1, pReleasedNode->retainCount() );
+	EXPECT_EQ( 1, pHolder->retainCount() );
+
+	//Now when we notify we have problem. We have use after release.
+	//It should crash
+	notifier.notify( getNotificationTestUseAfterRelease() );
+
+	pHolder->release();
 }
 
-TEST( Notifier, DISABLED_SpeedTestNotifyMultipleListeners )
-{
-	const int listenersCount = 1000;
-	Notifier* pNotifier = new Notifier();
-
-	//We want fresh notification center
-	CCNotificationCenter::sharedNotificationCenter()->purgeNotificationCenter();
-	CCNotificationCenter* pCenter =
-		CCNotificationCenter::sharedNotificationCenter();
-
-	vector<TestClazz*> listeners;
-
-	for( int i = 0; i < listenersCount; ++i )
-	{
-		int* pTab = new int[rand() % 1000 + 100];
-		listeners.push_back( new TestClazz() );
-		pNotifier->addNotification( getNotificationTest4(),
-									Utils::makeCallback( listeners.back(), &TestClazz::onDeliverNotification2 ) );
-
-		pCenter->addObserver( listeners.back(),
-							  callfuncO_selector( TestClazz::onOldNotificationFloat ), OLD_NOTIFICATION_4,
-							  nullptr );
-
-		delete pTab;
-	}
-
-	CC_PROFILER_PURGE_ALL();
-	CCFloat floatValue( 22.3F );
-
-	for( int i = 0; i < 100000; ++i )
-	{
-		CC_PROFILER_START( "Notifier_100k_notify_to_1k_listeners" );
-		pNotifier->notify( getNotificationTest4(), 22.3F );
-		CC_PROFILER_STOP( "Notifier_100k_notify_to_1k_listeners" );
-
-		CC_PROFILER_START( "CCNotificationCenter_100k_notify_to_1k_listeners" );
-		pCenter->postNotification( OLD_NOTIFICATION_4, &floatValue );
-		CC_PROFILER_STOP( "CCNotificationCenter_100k_notify_to_1k_listeners" );
-	}
-
-	CC_PROFILER_DISPLAY_TIMERS();
-
-	for( int i = 0; i < listenersCount; ++i )
-	{
-		delete listeners[i];
-	}
-}
-
-TEST( Notifier,
-	  DISABLED_SpeedTestNotifyMultipleListenersMultipleNotifications )
-{
-	const int listenersCount = 1000;
-	Notifier* pNotifier = new Notifier();
-
-	//We want fresh notification center
-	CCNotificationCenter::sharedNotificationCenter()->purgeNotificationCenter();
-	CCNotificationCenter* pCenter =
-		CCNotificationCenter::sharedNotificationCenter();
-
-	vector<TestClazz*> listeners;
-
-	for( int i = 0; i < listenersCount; ++i )
-	{
-		int* pTab = new int[rand() % 1000 + 100];
-		listeners.push_back( new TestClazz() );
-		int which = i % 4;
-
-		if( which == 0 )
-		{
-			pNotifier->addNotification( getNotificationTest1(),
-										Utils::makeCallback( listeners.back(), &TestClazz::onDeliverNotification ) );
-			pCenter->addObserver( listeners.back(),
-								  callfuncO_selector( TestClazz::onOldNotification ), OLD_NOTIFICATION_1,
-								  nullptr );
-		}
-		else if( which == 1 )
-		{
-			pNotifier->addNotification( getNotificationTest2(),
-										Utils::makeCallback( listeners.back(), &TestClazz::onDeliverNotification ) );
-			pCenter->addObserver( listeners.back(),
-								  callfuncO_selector( TestClazz::onOldNotification ), OLD_NOTIFICATION_2,
-								  nullptr );
-		}
-		else if( which == 2 )
-		{
-			pNotifier->addNotification( getNotificationTest3(),
-										Utils::makeCallback( listeners.back(), &TestClazz::onDeliverNotification3 ) );
-			pCenter->addObserver( listeners.back(),
-								  callfuncO_selector( TestClazz::onOldNotificationInt ), OLD_NOTIFICATION_3,
-								  nullptr );
-		}
-		else if( which == 3 )
-		{
-			pNotifier->addNotification( getNotificationTest4(),
-										Utils::makeCallback( listeners.back(), &TestClazz::onDeliverNotification2 ) );
-			pCenter->addObserver( listeners.back(),
-								  callfuncO_selector( TestClazz::onOldNotificationFloat ), OLD_NOTIFICATION_4,
-								  nullptr );
-		}
-
-
-		delete pTab;
-	}
-
-	CC_PROFILER_PURGE_ALL();
-
-	CCFloat floatValue( 22.3F );
-	CCInteger intValue( 99 );
-
-	for( int i = 0; i < 100000; ++i )
-	{
-		int which = i % 4;
-		CC_PROFILER_START( "Notifier_100k_notify_to_1k_listener_multiple_notifications" );
-
-		if( which == 0 )
-		{
-			pNotifier->notify( getNotificationTest1() );
-		}
-		else if( which == 1 )
-		{
-			pNotifier->notify( getNotificationTest2() );
-		}
-		else if( which == 2 )
-		{
-			pNotifier->notify( getNotificationTest3(), 99 );
-		}
-		else if( which == 3 )
-		{
-			pNotifier->notify( getNotificationTest4(), 23.3F );
-		}
-
-		CC_PROFILER_STOP( "Notifier_100k_notify_to_1k_listener_multiple_notifications" );
-		CC_PROFILER_START( "CCNotificationCenter_100k_notify_to_1k_listener_multiple_notifications" );
-
-		if( which == 0 )
-		{
-			pCenter->postNotification( OLD_NOTIFICATION_1 );
-		}
-		else if( which == 1 )
-		{
-			pCenter->postNotification( OLD_NOTIFICATION_2 );
-		}
-		else if( which == 2 )
-		{
-			pCenter->postNotification( OLD_NOTIFICATION_3, &intValue );
-		}
-		else if( which == 3 )
-		{
-			pCenter->postNotification( OLD_NOTIFICATION_4, &floatValue );
-		}
-
-		CC_PROFILER_STOP( "CCNotificationCenter_100k_notify_to_1k_listener_multiple_notifications" );
-	}
-
-	CC_PROFILER_DISPLAY_TIMERS();
-
-	for( int i = 0; i < listenersCount; ++i )
-	{
-		delete listeners[i];
-	}
-}
